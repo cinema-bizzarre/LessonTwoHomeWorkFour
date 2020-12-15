6c92c0ef7a9071a36d4fc1a472bef691eee51175
cinema-bizzarre <cinema-bizzarre@yandex.ru>
2020-12-16 01:24:01 +0300

    Выполнила дз к четвертому уроку

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextArea mainTextArea;
+
+    @FXML
+    TextField mainTextField;
+
+
+
+
+            public void btnOneClickAction(ActionEvent actionEvent){
+                System.out.println();
+            }
+
+    public void sendMessage(ActionEvent actionEvent) {
+    }
+}
